58ea0fbdf7ad2ba34cdc15f8df185cad0dc1be52
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2017-03-13 15:46:07 -0400

    created TodoComponent

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert('it works');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// create component
+var TodoComponent = React.createClass({
+  render: function() {
+    return(
+      <h1>Darryl</h1>
+    );
+  }
+});
+
+// put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
